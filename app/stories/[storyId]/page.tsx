a7f1a5b05bc0dfd308bdb5650b658c7d8141dc05
Henry <hennadii.lytvyn@gmail.com>
2025-11-14 22:37:37 +0000

    markup

--- app/stories/[storyId]/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { useParams } from 'next/navigation';
-
-export default function StoryPage() {
-  const params = useParams();
-  return <h2>StoryId – {params.storyId}</h2>;
-}
--- app/stories/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProtectedRoute from '@/components/ProtectedRoute/ProtectedRoute';
-
-export default function AddStoryPage() {
-  return (
-    <ProtectedRoute>
-      <h2>AddStory</h2>
-    </ProtectedRoute>
-  );
-}
