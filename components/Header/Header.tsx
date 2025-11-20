407fade464a8f76f5711e71e1d888bfb426d7d52
Serhii Haievoi <gaevuha@gmail.com>
2025-11-20 02:18:03 +0200

    fix button teme

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,3 @@
-// Components/Header/Header.tsx
-
 'use client';
 
 import { usePathname } from 'next/navigation';
@@ -11,12 +9,11 @@ import ThemeToggle from '../ThemeToggle/ThemeToggle';
 
 import { useMobileMenuOpen } from '@/lib/store/MobileMenuStore';
 import { useLockScroll } from '@/lib/hooks/useLockScroll';
-// import { useBreakpointStore } from '@/lib/store/breakpointStore';
-// import { useAuthStore } from '@/lib/store/authStore';
-// import PublishStoryLink from '../Navigation/PublishStoryLink/PublishStoryLink';
+import { useBreakpointStore } from '@/lib/store/breakpointStore';
 
 export default function Header() {
-  const isMainPage = usePathname() === '/';
+  const pathname = usePathname();
+  const isMainPage = pathname === '/';
 
   const isMobileMenuOpen = useMobileMenuOpen(state => state.isMobileMenuOpen);
   const setIsMobileMenuOpen = useMobileMenuOpen(
@@ -24,54 +21,42 @@ export default function Header() {
   );
   const closeMobileMenu = useMobileMenuOpen(state => state.closeMobileMenu);
 
-  // const isAuthenticated = useAuthStore(state => state.isAuthenticated);
-  // const device = useBreakpointStore(state => state.screenSize);
-  // const isTablet = device === 'tablet';
+  const device = useBreakpointStore(state => state.screenSize);
+  const isDesktop = device === 'desktop';
 
   useLockScroll(isMobileMenuOpen);
 
   let LogoProps: 'header-main-page' | 'mobile-menu-open' | 'footer' | undefined;
-
-  if (isMobileMenuOpen) {
-    LogoProps = 'mobile-menu-open';
-  }
-
-  if (!isMobileMenuOpen && isMainPage) LogoProps = 'header-main-page';
+  if (isMobileMenuOpen) LogoProps = 'mobile-menu-open';
+  else if (!isMobileMenuOpen && isMainPage) LogoProps = 'header-main-page';
 
   return (
-    <>
-      <header
-        className={`${css.header} ${isMainPage ? css.headerMainPage : ''}`}
-      >
-        <div className={`container ${css.headerContainer}`}>
-          <div className={css.logoWrapper}>
-            <Logo variant={LogoProps} handleClick={closeMobileMenu} />
-          </div>
+    <header className={`${css.header} ${isMainPage ? css.headerMainPage : ''}`}>
+      <div className={`container ${css.headerContainer}`}>
+        <div className={css.logoAndTheme}>
+          <Logo variant={LogoProps} handleClick={closeMobileMenu} />
+          <ThemeToggle variant={isMainPage ? 'header-main-page' : 'header'} />
+        </div>
+
+        {/* Навігація рендериться лише на десктопі */}
+        {isDesktop && (
           <div className={css.navWrapper}>
             <Navigation
               variant={isMainPage ? 'header-main-page' : 'header'}
               handleClick={closeMobileMenu}
             />
           </div>
-          {/* {isAuthenticated && isTablet && (
-            <li>
-              <PublishStoryLink
-                variant={isMainPage ? 'header-main-page' : undefined}
-              />
-            </li>
-          )} */}
-          <ThemeToggle variant={isMainPage ? 'header-main-page' : 'header'} />
+        )}
 
-          <div className={css.controls}>
-            {/* <ThemeToggle variant={isMainPage ? 'header-main-page' : 'header'} /> */}
-            <MobileMenuBtn
-              variant={isMainPage ? 'header-main-page' : undefined}
-              handleClick={setIsMobileMenuOpen}
-              isOpen={isMobileMenuOpen}
-            />
-          </div>
-        </div>
-      </header>
-    </>
+        {/* Мобільна кнопка меню */}
+        {!isDesktop && (
+          <MobileMenuBtn
+            variant={isMainPage ? 'header-main-page' : undefined}
+            handleClick={setIsMobileMenuOpen}
+            isOpen={isMobileMenuOpen}
+          />
+        )}
+      </div>
+    </header>
   );
 }
